9d0174b18e7d06dc69c571bf07163e4e7f2eea50
Schonus <128988493+Schonus@users.noreply.github.com>
2023-06-02 09:42:41 +0800

    modified:   HTML_CSS_JavaScript/Node_JS/fs/13-练习.js

--- HTML_CSS_JavaScript/Node_JS/fs/13-练习.js
+++ HTML_CSS_JavaScript/Node_JS/fs/13-练习.js
@@ -1,4 +1,21 @@
 const fs = require("fs");
-const ws = fs.createWriteStream(__dirname + "/观书有感.txt");
-const rs = fs.createReadStream(__dirname + "/观书有感2.txt");
-ws
+const files = fs.readdirSync('./code');
+console.log(files);
+files.forEach(items => {
+    //判断
+    let data = items.split("-");
+    var [num, name] = data;
+    if(Number(num) < 10)
+    {
+        num = 0 + num;
+    }
+    data = num + "-" + data[1];
+    console.log(data);
+    fs.rename("./code/" + items, data, err => {
+        if(err){
+            console.log("操作失败！");
+            return;
+        }
+    })
+})
+console.log("操作成功!");
