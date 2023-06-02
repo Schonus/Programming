57593191b59911856b73881174543aebea0c0658
Schonus <128988493+Schonus@users.noreply.github.com>
2023-06-03 07:18:18 +0800

    modified:   HTML_CSS_JavaScript/Node_JS/fs/13-练习.js

--- HTML_CSS_JavaScript/Node_JS/fs/13-练习.js
+++ HTML_CSS_JavaScript/Node_JS/fs/13-练习.js
@@ -3,18 +3,40 @@ const fs = require("fs");
 const files = fs.readdirSync('./code');
 console.log(files);
 var index = 0;
-files.forEach(item => {
+
+function rename(item,i) {
     //判断
     let data = item.split("-");
     var [num, name] = data;
-    if(Number(num) < 10)
-    {
-        num = String(++index);
-        num = 0 + num;
+    switch (i) {
+        case 0:
+            if(Number(num) < 10)
+            {
+                num = 0 + num;
+            }
+            break;
+        case 1:
+                
+                num = String(++index);
+                if(Number(num) < 10)
+                {
+                num = 0 + num;
+            }
+            break;
+        default:
+            break;
     }
     newName = num + "-" + data[1];
+    fs.renameSync(`./code/${item}`, `./code/${newName}`);
+    return newName;
+}
+        
+files.forEach(item => {
+    item = rename(item,0);
+})
+const files2 = fs.readdirSync('./code');
+files2.forEach(item => {
+    item = rename(item,1);
     console.log(item);
-    // console.log(newName);
-    // fs.renameSync(`./code/${item}`, `./code/${newName}`);
 })
 console.log("操作成功!");
