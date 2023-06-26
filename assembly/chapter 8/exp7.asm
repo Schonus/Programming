c03dcabdc56da5f4a0afd9f840668db8752bdc35
Schonus <128988493+Schonus@users.noreply.github.com>
2023-06-26 21:09:18 +0800

    modified:   assembly/chapter 8/1.asm 	new file:   assembly/chapter 8/exp7.asm

---
+++ assembly/chapter 8/exp7.asm
@@ -0,0 +1,68 @@
+assume cs:code,ds:data,es:table,ss:stack
+stack segment
+    dw 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+stack ends
+data segment
+    db '1975','1976','1977','1978','1979','1980','1981','1982','1983'
+    db '1984','1985','1986','1987','1988','1989','1990','1991','1992'
+    db '1983','1994','1995';以上是表示21年的21个字符串
+
+    dd 16,22,382,1356,2390,8000,16000,24486,50065,97479,140417,197514
+    dd 345980,590827,803530,1183000,1843000,2759000,3753000,4649000,5937000
+
+    dw 3,7,9,13,28,38,130,220,476,778,1001,1442,2258,2793,4037,5635,8226
+    dw 11542,14430,15257,17800
+data ends
+table segment
+    db 21 dup ('year summ ne ?? ')
+table ends
+code segment
+start:  mov ax,data
+        mov ds,ax
+        mov ax,table
+        mov es,ax
+        mov ax,stack
+        mov ss,ax
+        mov cx,15H
+        mov si,0
+        s:  push cx
+            mov bx,0
+            mov di,0
+            mov cx,2
+            s0: push cx
+                push si
+                mov cx,2
+                s01: mov ax,word ptr ds:[bx+si]
+                    mov word ptr es:[bp+di],ax
+                    add si,2
+                    add di,2
+                loop s01
+                mov byte ptr es:[bp+di],20H
+                inc di
+                add bx,84
+                pop si
+                pop cx
+            loop s0
+            push si
+            mov ax,si
+            mov cx,2
+            div cx
+            mov si,ax
+            mov ax,word ptr ds:[bx+si]
+            mov word ptr es:[bp+di],ax
+            add si,2
+            add di,2
+            mov byte ptr es:[bp+di],20H
+            add di,3
+            mov byte ptr es:[bp+di],20H
+            inc di
+            add bp,10H
+            pop si
+            add si,4
+            pop cx
+        loop s
+
+        mov ax,4C00H
+        int 21H
+code ends
+end start
